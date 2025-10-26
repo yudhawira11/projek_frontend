c417277c0f8a985fa37870a6b2788bb2225010d9
yudhawira11 <yudhajtz@gmail.com>
2025-10-27 00:12:26 +0700

    komit yudha

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,6 +19,7 @@
                         <a href="#" class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
                         <a href="#" class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
                     </div>
+
                     <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
                         <div>
                         <p class="text-sm text-gray-700">
